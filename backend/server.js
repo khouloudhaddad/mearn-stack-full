74665e218405378cc7a711a1b5fb098789586cfc
khouloudhaddad <khouloud.haddad@gmail.com>
2023-04-15 21:22:43 +0100

    Error Handling Middleware

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const port = process.env.PORT || 5000
-
+const {errorHandler} = require('./middleware/errorMiddleware')
 const app = express()
 
+//middleware
+app.use(express.json())
+//urlencoded 
+app.use(express.urlencoded({extended: false}))
+
 app.use('/api/goals', require('./routes/goalRoutes'));
+
+app.use(errorHandler)
 
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`)
