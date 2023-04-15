c7314f137bb619787785da0d88e8251f94a29442
khouloudhaddad <khouloud.haddad@gmail.com>
2023-04-15 13:41:39 +0100

    Test Get API with postman

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/api/goals', (req, res)=>{
+    res
+    .status(200)
+    .json({
+        message: "Get goals"
+    })
+} )
+
+app.listen(port, ()=>{
+    console.log(`Server started on port ${port}`)
+})
